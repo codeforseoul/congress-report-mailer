b6258852a081e1207b595af971c20dbd56d50728
hoony <thechunsik@gmail.com>
2015-12-10 16:20:45 +0900

    update db.py to add data to email template

--- db.py
+++ db.py
@@ -1,48 +1,61 @@
 import logging
 
 from pymongo import MongoClient
-from settings import MONGO_USERNAME, MONGO_PASSWORD
+from datetime import date, datetime
 
-client = MongoClient('localhost', 27017)
-db = client.congress_report
+from settings import MONGO_DB, MONGO_PORT, MONGO_USERNAME, MONGO_PASSWORD, DB_NAME
+
+# TODO: sign in to connect to mongodb
+client = MongoClient(MONGO_DB, MONGO_PORT)
+db = client[DB_NAME]
 
 attendance = db.attendance_results
 plenary = db.plenary_session_results
 
-def get_data_by_assembly(assembly, target):
-    if target == 'attendance':
-        db = attendance
-    elif target == 'plenary':
-        db = plenary
-        
-    return db.find_one({'assembly_id': assembly})
+today = date.today().replace(year = date.today().year - 1)
 
-def calc_rate_of_attendance(data):
-    try:
-        times = 0
-        for attend in data:
-            print(attend)
-            times = times + 1 if attend['attend'] == '출석' else times
-        rate = round(times/len(data), 2) * 100
-        return rate
+DATE_FROM = today.replace(day = 1)
+DATE_TO = DATE_FROM.replace(month = DATE_FROM.month + 1) if DATE_FROM.month < 12 else DATE_FROM.replace(year = today.year + 1, month = 1)
 
-    except Exception as e:
-        logging.exception(e)
-        return
-
-def get_rate_of_attendance(assembly):
-    data = get_data_by_assembly(assembly, 'attendance')
-    
-    main_attend = data['main_attend']
-    sub_attend = data['sub_attend']
-    
-    main_rate = calc_rate_of_attendance(main_attend)
-    sub_rate = calc_rate_of_attendance(sub_attend)
-    
+def classify_attendances(data):
+    # TODO: add more options such as '기권'
     return {
-        'main_rate': main_rate,
-        'sub_rate': sub_rate
+        'attendance': [session for session in data if session['attend'] == '출석'],
+        'absent': [session for session in data if session['attend'] != '출석']
     }
 
+def get_attends_on_month(data):
+    return [session for session in data
+        if (DATE_FROM <= datetime.utcfromtimestamp(session['date']/1000.0).date() and
+        DATE_TO > datetime.utcfromtimestamp(session['date']/1000.0).date())]
+
+def get_attendances_by_assembly(assembly):
+    data = attendance.find_one({'assembly_id': assembly})
+    
+    # TODO: error handling if result is None
+    main_attend = get_attends_on_month(data['main_attend'])
+    sub_attend = get_attends_on_month(data['sub_attend'])
+
+    main_sessions = classify_attendances(main_attend)
+    sub_sessions = classify_attendances(sub_attend)
+    
+    return {
+        'main_sessions': main_sessions,
+        'sub_sessions': sub_sessions
+    }
+    
+def get_vote_results_by_assembly(assembly):
+    votes = plenary.find({'처리날짜': {'$gt': DATE_FROM.isoformat(), '$lt': DATE_TO.isoformat()}})    
+    results = [{ 'title': v['의안명'], 'date': v['처리날짜'], 'result': result['type']} for v in votes for result in v['vote_results'] if assembly in result['member_idxs']]
+        
+    return results
+
+def convert_str_to_date(datestr):
+    return datetime.strptime(datestr, '%Y-%m-%d').date()
+
 if __name__ == '__main__':
-    print(get_rate_of_attendance(837))
+    # test
+    print('이번 달 출석 현황')
+    print(get_attendances_by_assembly(709))
+    print('이번 달 표결 현황')
+    print(get_vote_results_by_assembly(813))
--- settings.py
+++ settings.py
@@ -2,10 +2,13 @@
 
 import os
 
-__all__ = ['MONGO_USERNAME', 'MONGO_PASSWORD', 'IP', 'PORT', 'MANDRILL_KEY']
+__all__ = ['MONGO_DB', 'MONGO_PORT', 'MONGO_USERNAME', 'MONGO_PASSWORD', 'DB_NAME', 'IP', 'PORT', 'MANDRILL_KEY']
 
+MONGO_DB = 'localhost'
+MONGO_PORT = 27017
 MONGO_USERNAME = os.environ['mongoUsername']
 MONGO_PASSWORD = os.environ['mongoPwd']
+DB_NAME = 'congress_report'
 IP = '0.0.0.0'
 PORT = 8080
-MANDRILL_KEY = os.environ['mandrillKey']
+MANDRILL_KEY = 'test'
